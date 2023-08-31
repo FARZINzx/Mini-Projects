f390dd4ccdf6c547fa236d44c5964ca2a3093e66
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-08-31 15:36:59 +0330

    Aunt Husband Jokes --> set up script file and fetch url and get jole from api and set in jole element

--- Aunt Husband Jokes/script.js
+++ Aunt Husband Jokes/script.js
@@ -16,3 +16,31 @@ function getJoke(){
      .then((response)=>response.json())
      .then((data)=>jokeDom.innerHTML = data.joke)
 }
+
+// const apiKey = 'YOUR_GOOGLE_TRANSLATE_API_KEY';
+// const sourceText = 'Hello, how are you?';
+
+// const targetLanguage = 'es'; // Spanish
+
+// const apiUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+
+// const requestData = {
+//   q: sourceText,
+//   target: targetLanguage,
+// };
+
+// fetch(apiUrl, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(requestData),
+// })
+//   .then(response => response.json())
+//   .then(data => {
+//     const translatedText = data.data.translations[0].translatedText;
+//     console.log(translatedText);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });

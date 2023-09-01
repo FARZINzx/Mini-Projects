dbb49be5d1025246cf01b18bb31dbd189dfe620c
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-09-01 17:58:32 +0330

    creta get movie api and search ,movie api

--- Movie App/script.js
+++ Movie App/script.js
@@ -1,8 +1,8 @@
- const form = document.getElementById("form")
+const form = document.getElementById("form");
+const searchBar = document.getElementById("search");
 
- form.addEventListener("submit" , (event)=>{
-   event.preventDefault()
- })
+const API = "https://api.themoviedb.org/3/discover/movie";
+const API_SEARCH = "https://api.themoviedb.org/3/search/movie?8e64af0ec88ee556b3e4c2390f4fa1a5=&query=";
 
 const config = {
   method: "GET",
@@ -13,7 +13,28 @@ const config = {
   },
 };
 
-fetch('https://api.themoviedb.org/3/discover/movie',config)
-.then(response => response.json())
-.then(data => console.log(data.results))
-.catch(error => console.log(error.message))
+function apiUrl(url) {
+  fetch(url, config)
+    .then((response) => response.json())
+    .then((data) => console.log(data.results))
+    .catch((error) => console.log(error.message));
+}
+
+apiUrl(API)
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let searchVal = searchBar.value;
+
+  if(searchVal && searchVal !==''){
+    apiUrl(API_SEARCH + searchVal)
+    searchBar.value = " "
+  }else{
+    window.location.reload()
+   alert('Please enter a valid movie name')
+  }
+
+
+
+});

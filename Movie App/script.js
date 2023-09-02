00db9e7e1b8e424fab89192e3f5a514b59ff28d1
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-09-02 07:58:47 +0330

    movie app -->  get img and data  from api and set on DOM

--- Movie App/script.js
+++ Movie App/script.js
@@ -1,8 +1,11 @@
 const form = document.getElementById("form");
 const searchBar = document.getElementById("search");
+const main = document.getElementById("main");
 
 const API = "https://api.themoviedb.org/3/discover/movie";
-const API_SEARCH = "https://api.themoviedb.org/3/search/movie?8e64af0ec88ee556b3e4c2390f4fa1a5=&query=";
+const API_SEARCH =
+  "https://api.themoviedb.org/3/search/movie?8e64af0ec88ee556b3e4c2390f4fa1a5=&query=";
+const IMG_PATH = "https://image.tmdb.org/t/p/w500/";
 
 const config = {
   method: "GET",
@@ -16,25 +19,61 @@ const config = {
 function apiUrl(url) {
   fetch(url, config)
     .then((response) => response.json())
-    .then((data) => console.log(data.results))
+    .then((data) => showMovieHtml(data.results))
     .catch((error) => console.log(error.message));
 }
 
-apiUrl(API)
+function imgapi(id) {
+  const options = { method: "GET", headers: { accept: "application/json" } };
+
+  fetch(`https://api.themoviedb.org/3/movie/${id}/images`, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+}
+
+
+apiUrl(API);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let searchVal = searchBar.value;
 
-  if(searchVal && searchVal !==''){
-    apiUrl(API_SEARCH + searchVal)
-    searchBar.value = " "
-  }else{
-    window.location.reload()
-   alert('Please enter a valid movie name')
+  if (searchVal && searchVal !== "") {
+    apiUrl(API_SEARCH + searchVal);
+    searchBar.value = " ";
+  } else {
+    window.location.reload();
+    alert("Please enter a valid movie name");
   }
-
-
-
 });
+
+function showMovieHtml(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview,id } = movie;
+
+    console.log(movie);
+
+    const movieContainer = document.createElement("div");
+
+    movieContainer.classList.add("movie-container");
+
+    movieContainer.innerHTML = `
+    <div class="movie-img">
+          <img src="${IMG_PATH + poster_path}"/>
+        </div>
+        <div class="movie-detail">
+          <span class="movie-name">${title}</span>
+          <span class="movie-score">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>overview</h3>
+          <p>${overview}</p>
+        </div>
+    `;
+    main.appendChild(movieContainer);
+  });
+}
